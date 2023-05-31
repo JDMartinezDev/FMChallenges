e65efe99026688ff170749e914de4407bf7cc9d8
JDMartinezDev <h_juanmar@hotmail.com>
2023-05-30 21:31:34 -0600

    test2

--- interactive-rating-component/vite.config.js
+++ interactive-rating-component/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FMChallenges",
+  base: "/FMChallenges/interactive-rating-component",
   plugins: [react()],
 })
